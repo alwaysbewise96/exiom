f53c24f77e8792297d5c27182849097d4b92dda0
Staff0rd <2ndorb@gmail.com>
2024-08-14 09:42:05 +0700

    changes name

--- providers/offline-functions.sh
+++ providers/offline-functions.sh
@@ -35,3 +35,25 @@ reboot(){
     instance_name="$1"
     wsl reboot $(instance_id $instance_name)
 }
+
+create_instance() {
+	name="$1"
+	image_id="$2"
+	size_slug="$3"
+	region="$4"
+	boot_script="$5"
+  sshkey="$(cat "$base_dir/exiom.json" | jq -r '.sshkey')"
+  sshkey_fingerprint="$(ssh-keygen -l -E md5 -f ~/.ssh/$sshkey.pub | awk '{print $2}' | cut -d : -f 2-)"
+
+  keyid=$(cat ~/.ssh/exiom_rsa | grep "$sshkey_fingerprint" | awk '{ print $1 }')
+  curl -LO https://partner-images.canonical.com/core/focal/current/ubuntu-focal-core-cloudimg-amd64-root.tar.gz
+  wsl --import MyUbuntu C:\WSL\MyUbuntu ubuntu-focal-core-cloudimg-amd64-root.tar.gz --version 2
+  wsl -d MyExiom
+
+  sleep 260
+}
+
+instance_ip() {
+	name="$1"
+	instances | jq -r ".[]? | select(.name==\"$name\") | .networks.v4[]? | select(.type==\"public\") | .ip_address" | head -1
+}
